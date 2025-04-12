0a22b1e2c34aece6a0a19da59e90c38cd1ad0867
mokhtar0v <Omar.Mokhtar22@svnu.edu.eg>
2025-04-12 22:14:00 +0200

    Repository

--- MVCDemo/Demo.DataAccess/Repositories/DepartmentRepository.cs
+++ MVCDemo/Demo.DataAccess/Repositories/DepartmentRepository.cs
@@ -7,20 +7,44 @@ using System.Threading.Tasks;
 
 namespace Demo.DataAccess.Repositories
 {
-    internal class DepartmentRepository
+    public class DepartmentRepository(AppDbContext dbContext) : IDepartmentRepository
     {
-        private readonly AppDbContext _dbContext;
-
-        public DepartmentRepository(AppDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
+        private readonly AppDbContext _dbContext = dbContext;
+        //public DepartmentRepository(AppDbContext dbContext)
+        //{
+        //    _dbContext = dbContext;
+        //}
         //CRUD operations
+        //getAll
+        public IEnumerable<Department> getAll(bool withTracking = false)
+        {
+            if (withTracking) return _dbContext.Departments.ToList();
+            return _dbContext.Departments.AsNoTracking().ToList();
+        }
+        //getByID
         public Department? GetByID(int ID, AppDbContext dbContext)
         {
             var department = dbContext.Departments.Find(ID);
             return department;
 
+        }
+        //add
+        public int Add(Department dept)
+        {
+            _dbContext.Departments.Add(dept);
+            return _dbContext.SaveChanges();
+        }
+        //edit
+        public int Edit(Department dept)
+        {
+            _dbContext.Departments.Update(dept);
+            return _dbContext.SaveChanges();
+        }
+        //delete
+        public int Remove(Department dept)
+        {
+            _dbContext.Departments.Remove(dept);
+            return _dbContext.SaveChanges();
         }
     }
 }
--- MVCDemo/Demo.Presentation/Program.cs
+++ MVCDemo/Demo.Presentation/Program.cs
@@ -1,4 +1,6 @@
 using Demo.DataAccess.Contexts;
+using Demo.DataAccess.Models;
+using Demo.DataAccess.Repositories;
 using Microsoft.EntityFrameworkCore;
 using System;
 
@@ -12,12 +14,16 @@ namespace Demo.Presentation
 
             #region Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            //register service in DI container
             builder.Services.AddDbContext<AppDbContext>(options =>
             {
                 options.UseSqlServer(builder.Configuration["ConnectionStrings:DefaultConnection"]);
                 //options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
                 //options.UseSqlServer(builder.Configuration.GetSection("ConnectionStrings")["DefaultConnection"]);
-            }); //register service in DI container
+            });
+
+            builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
             #endregion
 
             var app = builder.Build();
