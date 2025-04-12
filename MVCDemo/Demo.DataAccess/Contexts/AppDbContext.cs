3f6f842eb9e5d5c2408b0338b4358435482c3d3a
mokhtar0v <Omar.Mokhtar22@svnu.edu.eg>
2025-04-12 13:51:26 +0200

    DepartmentModule

---
+++ MVCDemo/Demo.DataAccess/Contexts/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DataAccess.Contexts
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Connection string");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            //modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ MVCDemo/Demo.DataAccess/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DataAccess.Models
+{
+    internal class Department : BaseEntity
+    {
+        public string Name { get; set; } = null!;
+        public string Code { get; set; } = null!;
+        public string? description { get; set; }
+    }
+}
